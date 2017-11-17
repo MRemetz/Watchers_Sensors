0bda7cb65c774e84101fd36f8230f36e0e21f6ba
Max Remetz <max@remetz.com>
2017-11-17 16:14:12 -0500

    added code to watchers

--- watcher.erl
+++ watcher.erl
@@ -12,14 +12,19 @@ start() ->
 
 
 setup_loop(N, 1) ->
-	make_watcher(N || N <- lists:seq(N, 1));
+	make_watcher(N || N <- lists:seq(N, 1)i, N);
 
 setup_loop(N, Num_watchers) when N>10 ->
-	make_watcher([N || N <- lists:seq(N, N-9)]),
+	make_watcher([N || N <- lists:seq(N, N-9)], 10),
 	setup_loop(N-10, Num_watchers-1).
 
-make_watcher([]) ->
-	watcher();
+make_watcher(Sensor_list, 0) ->
+	watcher(Sensor_list);
 
-make_watcher(sensor_list)->
-	spawn_monitor()
+make_watcher(Sensor_list, N) when N>=1->
+	Snum = lists:nth(1, list),
+	{Pid, _} = spawn_monitor(?MODULE, sensor:sensor, [self()]),
+	make_watcher(lists:, N-1).
+
+
+
