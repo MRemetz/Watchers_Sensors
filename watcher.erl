db8e184947854429533b7dd9bfcde93d91878db7
Max Remetz <max@remetz.com>
2017-11-17 16:32:37 -0500

    continue fleshing out watcher

--- watcher.erl
+++ watcher.erl
@@ -12,19 +12,23 @@ start() ->
 
 
 setup_loop(N, 1) ->
-	make_watcher(N || N <- lists:seq(N, 1)i, N);
+	make_watcher([{N, P} || N <- lists:seq(N, 1), P <- [0], N);
 
 setup_loop(N, Num_watchers) when N>10 ->
-	make_watcher([N || N <- lists:seq(N, N-9)], 10),
+	make_watcher([{N, P} || N <- lists:seq(N, N-9), P <- [0]], 10),
 	setup_loop(N-10, Num_watchers-1).
 
 make_watcher(Sensor_list, 0) ->
+	%print sensor_list
 	watcher(Sensor_list);
 
 make_watcher(Sensor_list, N) when N>=1->
-	Snum = lists:nth(1, list),
+	Snum = lists:nth(N, list),
 	{Pid, _} = spawn_monitor(?MODULE, sensor:sensor, [self()]),
-	make_watcher(lists:, N-1).
+	make_watcher(lists:keyreplace(Snum, 1, Sensor_list, {Snum, Pid}), N-1).
 
-
+watcher(Sensor_list) ->
+	%receives indefnitely
+	%if receives 1-10, print
+	%if receives 11, spawn_monitor for that Snum, call itself with updates Sensor_list
 
