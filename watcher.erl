adaa29ccb1b705c2aa8183ff34e2adfffb512329
Max Remetz <max@remetz.com>
2017-11-19 12:13:07 -0500

    lists comprehension fixed

--- watcher.erl
+++ watcher.erl
@@ -1,10 +1,11 @@
 -module(watcher).
 -compile(export_all).
+-import(sensor, [sensor/2]).
 
 start() ->
-    {ok, [N]} = io:fread("enter number of sensors> ", "~d") ,
+    {ok, [N]} = io:fread("enter number of sensors> ", "~d"),
     if N =< 1 ->
-          io:fwrite("setup: range must be at least 2~n", []) ;
+          io:fwrite("setup: range must be at least 2~n", []);
        true -> 
          Num_watchers = 1 + (N div 10),
          setup_loop(N, Num_watchers) 
@@ -12,23 +13,27 @@ start() ->
 
 
 setup_loop(N, 1) ->
-	make_watcher([{N, P} || N <- lists:seq(N, 1), P <- [0], N);
+	Wid = spawn(?MODULE, make_watcher, [self(), [{X, Y} || X <- lists:seq(1, N), Y <- [0]], N]);
 
 setup_loop(N, Num_watchers) when N>10 ->
-	make_watcher([{N, P} || N <- lists:seq(N, N-9), P <- [0]], 10),
+	Wid = spawn(?MODULE, make_watcher, [self(), [{X, Y} || X <- lists:seq(N-9, N), Y <-
+														   [0]], 10]),
 	setup_loop(N-10, Num_watchers-1).
 
-make_watcher(Sensor_list, 0) ->
+make_watcher(Wid, Sensor_list, 0) ->
 	%print sensor_list
 	watcher(Sensor_list);
 
-make_watcher(Sensor_list, N) when N>=1->
-	Snum = lists:nth(N, list),
-	{Pid, _} = spawn_monitor(?MODULE, sensor:sensor, [self()]),
-	make_watcher(lists:keyreplace(Snum, 1, Sensor_list, {Snum, Pid}), N-1).
+make_watcher(Wid, Sensor_list, N) when N>=1->
+	Sid = lists:nth(N, list),
+	{Pid, _} = spawn_monitor(?MODULE, sensor, [self(), Wid]),
+	make_watcher(Wid, lists:keyreplace(Sid, 1, Sensor_list, {Sid, Pid}), N-1).
 
 watcher(Sensor_list) ->
+	io:format("watcher").
 	%receives indefnitely
-	%if receives 1-10, print
-	%if receives 11, spawn_monitor for that Snum, call itself with updates Sensor_list
+	%if receives 1-10, print sensor number and measurement
+	%if receives crash, print sensor number, and crash
+	%	spawn_monitor for that Sid, print updated list
+	%	call itself with updated Sensor_list
 
