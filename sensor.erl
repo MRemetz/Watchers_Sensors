9efedb66286c4714de2defd232ea61998d512fe9
Max Remetz <max@remetz.com>
2017-11-22 13:35:57 -0500

    added sensor code

--- sensor.erl
+++ sensor.erl
@@ -2,5 +2,11 @@
 -compile(export_all).
 
 sensor(Wid, Sid) ->
-	io:format("Wid: ~w~n", [Wid]),
-	io:format("Sid: ~w~n", [Sid]).
+	Sleep_time = rand:uniform(10000),
+	timer:sleep(Sleep_time),
+	Measurement = rand:uniform(11),
+	case Measurement of
+		11 -> exit("Sensor ~w~d crashed", [Sid]);
+		_ok -> Wid!{Sid, Measurement},
+			   sensor(Wid, Sid)
+	end.
